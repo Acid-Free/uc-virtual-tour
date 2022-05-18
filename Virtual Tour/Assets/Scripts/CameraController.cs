3885c430d0ee6f047cd59d6c9a8d9e5e418873c8
Sian Caleb Domasig <siancaleb@gmail.com>
2022-05-18 13:05:51 +0800

    Add animated sprites for move button

--- Virtual Tour/Assets/Scripts/CameraController.cs
+++ Virtual Tour/Assets/Scripts/CameraController.cs
@@ -6,7 +6,15 @@ public class CameraController : MonoBehaviour
 {
     [SerializeField] float rotateSpeed = 300.0f;
     [SerializeField] float zoomSpeed = 600.0f;
+    [SerializeField] float minFieldOfView = 40.0f;
+    [SerializeField] float maxFieldOfView = 110.0f;
+    [SerializeField] float defaultFieldOfView = 60.0f;
     float fieldOfView = 60.0f;
+
+    void Start()
+    {
+        fieldOfView = defaultFieldOfView;
+    }
 
     void Update()
     {
@@ -19,7 +27,7 @@ public class CameraController : MonoBehaviour
         if (Input.GetMouseButton(1) || Input.GetMouseButton(2))
         {
             // move camera forward/backward
-            fieldOfView = Mathf.Clamp(fieldOfView + Input.GetAxis("Mouse Y") * Time.deltaTime * zoomSpeed, 40.0f, 90.0f);
+            fieldOfView = Mathf.Clamp(fieldOfView + Input.GetAxis("Mouse Y") * Time.deltaTime * zoomSpeed, minFieldOfView, maxFieldOfView);
             Camera.main.fieldOfView = fieldOfView;
         }
     }
